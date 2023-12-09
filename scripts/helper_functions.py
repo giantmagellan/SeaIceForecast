a8e272a5b4ca05d684398d1863972b3bb7ea67b0
rudy <bryanflores@sandiego.edu>
2023-12-08 21:01:48 -0800

    seasonal forecast dev

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 
 def convert_column_names(df):
     """
@@ -105,3 +107,29 @@ def standard_formatter(df):
 
 # TODO: Report summary statisitcs
 # TODO: Eliminate duplicate dates
+
+def calculate_rmse(outcome_var, forecast):
+    """
+    Calculate RMSE.
+    :param outcome_var: series, variable to be forecasted.
+    :param forecast: series, forecasted series.
+    :return rmse: float, root mean squared error
+    """
+    return np.sqrt(mean_squared_error(outcome_var, forecast)).round(2)
+
+def calculate_mape(outcome_var, forecast):
+    """
+    Calculate MAPE.
+    :param outcome_var: series, variable to be forecasted.
+    :param forecast: series, forecasted series.
+    :return mape: float, mean absolute percentage error
+    """
+    return np.round(np.mean(np.abs(outcome_var - forecast) / outcome_var) * 100, 2)
+
+# TODO: iterate over all forecasted metrics and output as single dataframe
+def eval_metrics(test_set, forecast_set):
+
+    rmse = calculate_rmse(test_set, forecast_set)
+    mape = calculate_mape(test_set, forecast_set)
+
+    return rmse, mape
